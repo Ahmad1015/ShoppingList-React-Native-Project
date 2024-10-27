6f9cbb09685f35f5cbba03f732787f074d379a09
Ahmad <mahmedraza90@gmail.com>
2024-10-27 15:28:47 +0500

    Initial commit

--- ios/Shopping_ListTests/Shopping_ListTests.m
+++ ios/Shopping_ListTests/Shopping_ListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ShoppingListAppTests : XCTestCase
+@interface Shopping_ListTests : XCTestCase
 
 @end
 
-@implementation ShoppingListAppTests
+@implementation Shopping_ListTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
